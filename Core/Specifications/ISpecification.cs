ac78944df5e781828ad8e17ac6018f4e68c0a2e2
ManfredBrugger <manfredabrugger@gmail.com>
2023-12-14 11:51:43 -0600

    End of section 6

--- Core/Specifications/ISpecification.cs
+++ Core/Specifications/ISpecification.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
@@ -10,5 +11,13 @@ namespace Core.Specifications
     {
         Expression<Func<T, bool>> Criteria {get; }
         List<Expression<Func<T, object>>> Includes {get; }
+        Expression<Func<T, object>> OrderBy {get; }
+        Expression<Func<T, object>> OrderByDescending {get; }
+
+        int Take {get; }
+
+        int Skip {get; }
+
+        bool isPagingEnabled{get; }
     }
 }
